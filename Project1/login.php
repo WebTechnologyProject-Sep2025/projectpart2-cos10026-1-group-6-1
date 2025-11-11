d44b227127ad0625fab786b1fa84c3c90a08da39
VuDuyAnh122 <106216120@student.swin.edu.au>
2025-11-11 08:59:40 +0700

    added and modified config.php, modified login.php, code are not tested

--- Project1/login.php
+++ Project1/login.php
@@ -6,3 +6,25 @@ if(isset($_SESSION['user_id'])){
 }
 $errormsg = $_GET['error'] ?? ''
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HR LOGIN</title>
+</head>
+<body>
+    <h1>EOI DATABASE ACCESS</h1>
+    <?php if ($errormsg): ?>
+        <p><?php echo htmlspecialchar($errormsg) ?></p>
+    <?php endif ?>
+    <form action="login_process.php" method="post" class="login_proc">
+        <label for="">Username</label>
+        <input type="text" name="username" required> <br>
+        <label for="">Password</label>
+        <input type="password" name="password" required> <br>
+        <button type="submit">Login</button>
+    </form>
+    
+</body>
+</html>
