24c759ceb3c93db213fe68c873f1d1ebf85c7da0
khaibatau-cn <kevinlamkhai7@gmail.com>
2025-11-15 00:44:45 +0700

    Added lock if max attempts reached limit feature for both users, auto mark creation time for user, last login time for user and hr_user.

--- Project1/hash_generator.php
+++
@@ -1,6 +0,0 @@
-<?php
-$plain_password = "quan2007"; 
-$hashed_password = password_hash($plain_password, PASSWORD_DEFAULT);
-echo "Plain Password: " . $plain_password . "<br>";
-echo "Generated Hash: <strong>" . $hashed_password . "</strong><br>";
-?>
