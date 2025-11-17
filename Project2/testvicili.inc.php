752240748874af82a011a603bde0c5583e198f31
khaibatau-cn <kevinlamkhai7@gmail.com>
2025-11-17 20:53:48 +0700

    Added welcome message

--- Project2/testvicili.inc.php
+++
@@ -1,5 +0,0 @@
-    <?php if (isset($_SESSION['hr_user_id'])): ?>
-      <h2 id="welcome_hr">Welcome, HR User <?php echo htmlspecialchars($_SESSION['hr_user_id']); ?></h2>
-    <?php elseif (isset($_SESSION['user_id'])): ?>
-      <h2 id="welcome_user">Welcome, User <?php echo htmlspecialchars($_SESSION['user_id']); ?></h2>
-    <?php endif; ?>
